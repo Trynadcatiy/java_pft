9b260de2f85e351639589a0950a90e110cc09e5a
Pavel Voronin <@trynadcatiy>
2019-01-03 17:27:40 +0300

    Добавлен класс Point и функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,21 @@ public class MyFirstProgram {
         hello("user");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадб прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадб прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 5);
+        System.out.println("Расстояние от точки p1 до p2 = " + distance(p1, p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(Square s) {
-        return s.l * s.l;
-
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
     }
-
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
-
 }
