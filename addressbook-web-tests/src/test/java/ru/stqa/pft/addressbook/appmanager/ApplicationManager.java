26e86e4ac967351014251bd4ce6ede8ee3248d53
Pavel Voronin <@trynadcatiy>
2019-01-07 17:16:06 +0300

    Создан базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -29,30 +26,8 @@ public class ApplicationManager {
     }
 
     public void stop() {
-        logout();
+        sessionHelper.logout();
         wd.quit();
-    }
-
-    public void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-
-    public boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 
     public GroupHelper getGroupHelper() {
