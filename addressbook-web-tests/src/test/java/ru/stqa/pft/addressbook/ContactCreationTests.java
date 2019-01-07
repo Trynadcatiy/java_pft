e959e96c425e010a3c549b5cd4561a98642fde16
Pavel Voronin <@trynadcatiy>
2019-01-07 15:58:23 +0300

    Создан родителский класс TestBase. Все методы тестов вынесены в родительский класс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -8,17 +8,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ContactCreationTests {
-    private WebDriver wd;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
+public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
@@ -27,81 +17,5 @@ public class ContactCreationTests {
         submitContactCreation();
         returnToHomePage();
     }
-
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        logout();
-        wd.quit();
-    }
-
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private void login(String userName, String password) {
-        wd.findElement(By.name("user")).sendKeys(userName);
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    private void returnToHomePage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitContactCreation() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-        wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("home")).sendKeys(contactData.getHomeTelephone());
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobileTelephone());
-        wd.findElement(By.name("work")).sendKeys(contactData.getWorkTelephone());
-        wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-        wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
-        wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
-        wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
-        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-        wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
-        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
-        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAmonth());
-        wd.findElement(By.name("ayear")).sendKeys(contactData.getAyear());
-        new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
-        wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
-        wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
-        wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
-    }
-
-    private void initContactCreation() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
 
 }
