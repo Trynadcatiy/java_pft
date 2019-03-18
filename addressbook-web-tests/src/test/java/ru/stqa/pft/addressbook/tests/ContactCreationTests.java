918b912cc413f31c4897d872aa72dca8558b1186
Pavel Voronin <@trynadcatiy>
2019-03-18 11:08:26 +0300

    Браузер  запускается однократно для тестсьюта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void testContactCreation() {
 
         ContactData contact = new ContactData("Pavel", "Petrovich", "Voronin", "Trynadcatiy",
