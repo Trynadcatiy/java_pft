b6ebfc18efa07b6f174bd682a4a120a1a8bc18b6
Pavel Voronin <@trynadcatiy>
2019-03-20 13:18:16 +0300

    Добавлен класс для тестирования номеров контакта.В класс добавлена проверка предусловий наличия хотябы одного контакта. Добавлен метод получения телефонов со страницы реадактирования контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactPhoneTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.goTo().homePage();
+        if (app.contact().all().size() == 0){
+            app.contact().create(new ContactData()
+                    .withFirstname("Pavel").withMiddlename("Petrovich").withLastname("Voronin")
+                    .withNickname("Trynadcatiy").withTitle("Test").withCompany("DXBX")
+                    .withAddress("SPb").withHomeTelephone("12-34-567").withMobileTelephone("+79003002000")
+                    .withWorkTelephone("+79003002001").withFax("79003002002").withEmail("p.voronin@fakemail.ru")
+                    .withEmail2("test1@fakemail.ru").withEmail3("test2@fakemail.ru").withHomepage("dxbx.ru").withBday("1")
+                    .withBmonth("January").withByear("1987").withAday("2").withAmonth("February").withAyear("1988")
+                    .withAddress2("Test16").withPhone2("Test17").withNotes("Test18").withGroup("Test7"));
+        }
+    }
+
+    @Test
+    public void testContactPhone(){
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    }
+}
