57a045f9014bb75a692bbd4f7534a0243ea5b631
Pavel Voronin <@trynadcatiy>
2019-04-04 20:34:48 +0300

    Добавлено логирование запуска и прерывания теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -18,6 +27,16 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] parametrs) {
+        logger.info("Start test " + method.getName() + "with parametrs" + Arrays.asList(parametrs));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method method) {
+        logger.info("Stop test " + method.getName());
     }
 
 }
